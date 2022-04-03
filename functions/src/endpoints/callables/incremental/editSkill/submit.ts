fb1e8a941dcfbc726beb087aca9f2dbf44c6dc20
nabildroid <pni20156789@gmail.com>
2022-04-03 15:52:55 +0100

    batch update

---
+++ functions/src/endpoints/callables/incremental/editSkill/submit.ts
@@ -0,0 +1,39 @@
+import * as functions from "firebase-functions";
+
+import { IncrementalData } from "../../../../types";
+import Redirect from "../../../../core/redirect";
+import Form, { FormInput } from "../../../../core/form";
+import { EditSkillForm } from "./utils";
+
+interface NavigationData extends IncrementalData {
+  action: string;
+  inputs: FormInput[];
+  actionButton: FormInput;
+  id: string;
+}
+
+export default functions.https.onCall(async (data: NavigationData) => {
+  const homePage = await Redirect.load({
+    cookies: data.cookies,
+    weirdData: data.weirdData,
+    from:
+      data.from ??
+      "https://noor.moe.gov.sa/Noor/EduWavek12Portal/HomePage.aspx",
+  });
+
+  const form = new EditSkillForm(
+    Form.fromJson({
+      action: data.action,
+      weird: data.weirdData,
+      inputs: data.inputs,
+      actionButtons: [data.actionButton],
+    }).html
+  );
+
+  const search = await form.submit(data.actionButton.name!, homePage);
+
+  form.updateFromSreachSubmission(search);
+
+  // todo include the cookies and redirected;
+  return { form: form.toJson() };
+});
