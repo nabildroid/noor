28bfc38af427de4479900e88fff54e02f1e82262
nabildroid <pni20156789@gmail.com>
2022-05-18 20:02:02 +0100

    contact landing page

--- dashboard/build.js
+++ dashboard/build.js
@@ -1,14 +1,13 @@
 const fs = require("fs");
 
-const template = fs.readFileSync("./build.html","utf-8");
+const template = fs.readFileSync("./build.html", "utf-8");
+const templateContact = fs.readFileSync("./contact.html", "utf-8");
+
 const { html, style } = JSON.parse(fs.readFileSync("./data.html"));
 
 const output = template
   .replace("<style></style>", `<style>${style}</style>`)
-  .replace("<content></content>", html);
-
+  .replace("<content></content>", html)
+  .replace("<contact></contact>", templateContact);
 
 fs.writeFileSync("./public/index.html", output);
-
-
-
