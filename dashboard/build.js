9aed0bc96a7a50ed7841a56e4c4e4a3cddea65b6
nabildroid <pni20156789@gmail.com>
2022-05-18 20:16:20 +0100

    fix bug

--- dashboard/build.js
+++ dashboard/build.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 const template = fs.readFileSync("./build.html", "utf-8");
 const templateContact = fs.readFileSync("./contact.html", "utf-8");
 
+console.log(templateContact);
 const { html, style } = JSON.parse(fs.readFileSync("./data.html"));
 
 const output = template
   .replace("<style></style>", `<style>${style}</style>`)
-  .replace("<content></content>", html)
-  .replace("<contact></contact>", templateContact);
+  .replace("<contact></contact>", templateContact)
+  .replace("<content></content>", html);
 
 fs.writeFileSync("./public/index.html", output);
